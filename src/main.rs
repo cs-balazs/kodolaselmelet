9747a170af305292332b194ed33ab84dbb0bea66
Csete Balázs <csetebalu@gmail.com>
2022-09-27 09:56:06 +0200

    primality

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::str::FromStr;
 fn main() {
     println!(
         "{}",
-        kodolaselmelet::primality::miller_rabin(Integer::from_str("173874373").unwrap())
+        kodolaselmelet::primality::miller_rabin(&Integer::from_str("3").unwrap())
     );
 }
--- src/primality.rs
+++ src/primality.rs
@@ -1,60 +1,51 @@
-use crate::exponentiation::{self, binary};
+use crate::exponentiation::binary;
 use rug::{rand::RandState, Integer};
-use std::ops::{AddAssign, MulAssign};
+use std::ops::AddAssign;
 
 fn n_factors(n: &Integer) -> Integer {
-    if n % Integer::from(2u64) == Integer::from(1u64) {
+    if n % Integer::from(2u64) == 1 {
         return Integer::from(0u64);
     }
     let mut exp = Integer::from(1u64);
     let mut n_clone = n.clone();
-    while &n_clone % Integer::from(2u64) == Integer::from(0u64) {
+    while &n_clone % Integer::from(2u64) == 0 {
         n_clone /= Integer::from(2u64);
         exp += Integer::from(1u64);
     }
 
-    return exp - Integer::from(1u64);
+    exp - 1
 }
 
-pub fn miller_rabin(n: Integer) -> bool {
-    if &n % Integer::from(2u64) == Integer::from(0u64) {
+pub fn miller_rabin(n: &Integer) -> bool {
+    if *n == 2 {
+        return true;
+    }
+    if n % Integer::from(2) == 0 || *n == 1 {
         return false;
     }
-    let n_minus_one = &n - Integer::from(1u64);
+
+    let n_minus_one = n - Integer::from(1);
     let k = n_factors(&n_minus_one);
-    let r = &n_minus_one / (exponentiation::binary(&Integer::from(2u64), &k));
+    let mut exp = binary(&Integer::from(2), &k, None);
+    let r = &n_minus_one / exp.clone();
 
-    println!("n = {}", n);
-    println!("k = {}", k);
-    println!("r = {}", r);
+    let mut rng = RandState::new();
+    let a = n - Integer::from(2);
+    let mut a = a.random_below(&mut rng);
+    a.add_assign(Integer::from(1));
 
-    let mut i = Integer::from(0u64);
-    while i < k {
-        println!("{}", i);
-        let mut rng = RandState::new();
-        let random = &n - Integer::from(3);
-        let mut random = random.random_below(&mut rng);
-        random.add_assign(Integer::from(2));
-        println!("random = {}", random);
-        println!("r = {}", r);
-        let mut x = binary(&random, &r) % &n;
-        println!("x = {}", x);
+    while exp > 0 {
+        let expp = r.clone() * exp.clone();
+        let res = binary(&a, &expp, Some(n));
 
-        if x == Integer::from(1u64) || x == n_minus_one {
-            i += Integer::from(1u64);
+        if res == 1 {
+            exp >>= 1;
             continue;
-        }
+        };
 
-        let mut j = Integer::from(1u64);
-
-        while j <= &k - Integer::from(1u64) {
-            x.mul_assign(x.clone());
-            x.mod_u(n.to_u32_wrapping());
-            if x == n_minus_one {
-                i += Integer::from(1u64);
-                continue;
-            }
-            j += Integer::from(1u64);
+        // Checking for n - 1, which is -1
+        if res == n_minus_one {
+            break;
         }
 
         return false;
@@ -65,14 +56,41 @@ pub fn miller_rabin(n: Integer) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use crate::primality::miller_rabin;
     use rug::Integer;
 
     #[test]
     fn test() {
-        assert!(miller_rabin(Integer::from(7u64)));
-        assert!(!miller_rabin(Integer::from(561u64)));
-        assert!(!miller_rabin(Integer::from(56112312u64)));
-        assert!(!miller_rabin(Integer::from(56384756283764451u64)));
+        let primes = [
+            "2",
+            "3",
+            "5",
+            "7",
+            "543165915552570104247678780070604672279011289359839209727583364406691873327606394089689340382741755070311804128765079618964382566208339179079459485225742676430588575604511729456129994320325905584854716158199748699700993344792840061111419166698246900964457709548045917326301847809524228386681597635267",
+            "691826793068458536074208355133049291478531419048941848702313338608164245322895819651978896147719733508565736653903956362824739385732131890969671002559232884926169408928219198718725581288282235796285353558468100964394244945998514012276023216327756247926340827782709941975233386336751116416100943416897",
+            "720714523785889173191354177371045760242297569261140563779852769239494236607878516643196470647354242607091192426841274197616849531050433864775643197014575443597601363224005309228633349876258651710793439200905812200126442288004063350129579058857449170104638055254079167970988227634912890827343610996771",
+            "654192561342273737540525596287228522458866748209355344196805430932937858733575088437483439928216182954804138665208507999277928799546663114534323365043147989308493599740693163912846408255526501434571396592122908458907213628524101013797685469295239805265510171803657372870466979387633939198951173246927",
+            "983389828069245665884547151859586238260145204546001744760748176701110103071142463342218818934867896489832127332118010158891753427831379000978255113915046040935791699717694614182190446666573340390308631860645247204614492426734524967861717830533351482447223474314657448327342722551224532940073042873021"
+        ];
+
+        let non_primes = [
+            "1",
+            "9",
+            "516",
+            "798726597823569276598027",
+            "19374971848167",
+        ];
+
+        for prime in primes {
+            println!("Testing {}", prime);
+            assert!(miller_rabin(&Integer::from_str(&prime).unwrap()))
+        }
+
+        for non_prime in non_primes {
+            println!("Testing {}", non_prime);
+            assert!(!miller_rabin(&Integer::from_str(&non_prime).unwrap()))
+        }
     }
 }
