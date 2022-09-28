b36d1c9aea773b24a9fcf0d8adddc229e79acc66
Csete Balázs <csetebalu@gmail.com>
2022-09-28 20:38:41 +0200

    optional e param

--- src/rsa.rs
+++ src/rsa.rs
@@ -37,14 +37,20 @@ pub struct KeyPair {
     pub privkey: Integer,
 }
 
-pub fn generate_keys(p: &Integer, q: &Integer) -> Result<KeyPair, NotPrimeError> {
+pub fn generate_keys(
+    p: &Integer,
+    q: &Integer,
+    e: Option<&Integer>,
+) -> Result<KeyPair, NotPrimeError> {
     if !miller_rabin(p) || !miller_rabin(q) {
         return Err(NotPrimeError);
     }
     let n = (p * q).complete();
     let lambda_n = (p - Integer::from(1)).lcm(&(q - Integer::from(1)));
     let mut rng = RandState::new();
-    let e: Integer = {
+    let e: Integer = if let Some(param_e) = e {
+        param_e.clone()
+    } else {
         let mut e: Integer;
         loop {
             e = (&lambda_n - Integer::from(1))
@@ -102,6 +108,7 @@ fn str_to_bigint(s: &str) -> Integer {
     )
     .unwrap()
 }
+
 fn bigint_to_string(bigint: &Integer) -> String {
     let string = bigint.to_string();
     let num_of_zeroes_to_prepend = string.len() % 3;
@@ -161,6 +168,7 @@ mod tests {
         let keys = generate_keys(
             &Integer::from_str("4").unwrap(),
             &Integer::from_str("7").unwrap(),
+            None,
         );
 
         if let Ok(_) = keys {
@@ -182,6 +190,7 @@ mod tests {
         let keys = generate_keys(
             &Integer::from_str("691826793068458536074208355133049291478531419048941848702313338608164245322895819651978896147719733508565736653903956362824739385732131890969671002559232884926169408928219198718725581288282235796285353558468100964394244945998514012276023216327756247926340827782709941975233386336751116416100943416897").unwrap(),
             &Integer::from_str("720714523785889173191354177371045760242297569261140563779852769239494236607878516643196470647354242607091192426841274197616849531050433864775643197014575443597601363224005309228633349876258651710793439200905812200126442288004063350129579058857449170104638055254079167970988227634912890827343610996771").unwrap(),
+            None
         ).unwrap();
 
         for message in messages {
@@ -204,6 +213,7 @@ mod tests {
         let keys = generate_keys(
             &Integer::from_str("691826793068458536074208355133049291478531419048941848702313338608164245322895819651978896147719733508565736653903956362824739385732131890969671002559232884926169408928219198718725581288282235796285353558468100964394244945998514012276023216327756247926340827782709941975233386336751116416100943416897").unwrap(),
             &Integer::from_str("720714523785889173191354177371045760242297569261140563779852769239494236607878516643196470647354242607091192426841274197616849531050433864775643197014575443597601363224005309228633349876258651710793439200905812200126442288004063350129579058857449170104638055254079167970988227634912890827343610996771").unwrap(),
+            None
         ).unwrap();
 
         for message in messages {
